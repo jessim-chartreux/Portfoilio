48bb2003c71865edfc19a01fe73f1559ae1de5f3
jessim-chartreux <jessim.chartreux@gmail.com>
2024-10-18 11:41:54 +0000

    sdsdsd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,8 @@ const Header = () => {
       <NavLink to="/"> <img src="https://cdn.discordapp.com/attachments/1129209624810242078/1234505987591901216/1713864554443.png?ex=666e488b&is=666cf70b&hm=fd6d03ae28c088fd923bb9579ba788615fba297e0fc314ae808ee9cde7409573&" alt='logo'/></NavLink>
       <div className="link-right-container">
         <a href="https://discord.gg/" target="_blank" rel="noopener noreferrer">Discord</a>
-        <NavLink to="/Render"target="_blank" activeClassName="active">CV</NavLink>
+        <NavLink to="/Render" target="_blank" activeClassName="active">CV</NavLink>
+        <a href="/Render"> CVV</a>
         <NavLink to="/Contact" activeClassName="active">Contact</NavLink>
       </div>
     </header>
