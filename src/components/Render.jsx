561ab40c8bec39fa32e89f3b4bcce19f78759311
jessim-chartreux <jessim.chartreux@gmail.com>
2024-10-18 11:38:08 +0000

    sds

--- src/components/Render.jsx
+++ src/components/Render.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+// import '../App.css'; 
+import CV_Jessim from './CV_Jessim.pdf';
 import Cvhead from './Cvhead';
 
 function Render() {
-    return (
-        <div>
-            <Cvhead />
-            <embed src="/CV_Jessim.pdf" type="application/pdf" width="100%" height="1200px" />
-        </div>
+    return(
+       <div>
+           <Cvhead />
+           <a href={CV_Jessim} download="CV_Jessim.pdf">
+               <button>Télécharger mon CV</button>
+           </a>
+       </div>
     );
 }
 
